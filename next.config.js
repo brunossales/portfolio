d3457486b7648259a31e82a9de2008d9e9c434d6
brunossales <bruno.particular25@hotmail.com>
2023-12-13 14:31:15 -0300

    ci: implement dynamic import to lucide-react

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    transpilePackages: ['lucide-react'],
     reactStrictMode: true,
     images: {
         domains: [
             'localhost:3000',
             'avatars.githubusercontent.com',
             'portfolio-brunossales.vercel.app',
-        ],
+        ]
     },
 };
 
