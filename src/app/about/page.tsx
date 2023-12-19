48febaa3de3f12e1c3206ff8bfd2762e60e2af28
Bruno Sales <bruno.particular25@hotmail.com>
2023-12-19 13:08:58 -0300

    feat: lint and motion-framer

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 
+import { Text } from '@/components/atoms/Text';
+import { useFindUserDataHook } from '@/hooks/find-user-data.hook';
+import { motion } from 'framer-motion';
+import { User } from 'lucide-react';
 import Image from 'next/image';
+import { useMemo } from 'react';
+import androidImagePng from '../../../public/android.png';
 import nextImagePng from '../../../public/next.png';
 import reactImagePng from '../../../public/react.png';
-import androidImagePng from '../../../public/android.png';
-import { useFindUserDataHook } from '@/hooks/find-user-data.hook';
-import { User } from 'lucide-react';
-import { useMemo } from 'react';
-import { Text } from '@/components/atoms/Text';
 
 export default function About() {
     const { data: dataUser } = useFindUserDataHook();
@@ -17,9 +18,14 @@ export default function About() {
     }, [dataUser]);
 
     return (
-        <div className="flex flex-col items-start p-10 gap-5 w-2/3">
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="flex flex-col items-start p-10 gap-5 w-2/3"
+        >
             <div className="flex items-center justify-center gap-3">
-                <Text size='4xl' variant='primary' forceBold text='SOBRE MIM' />
+                <Text size="4xl" variant="primary" forceBold text="SOBRE MIM" />
 
                 {avatarUrl ? (
                     <Image
@@ -34,18 +40,22 @@ export default function About() {
                 )}
             </div>
 
-            <Text variant='secondary' forceBold size='2xl' text='Me considero uma pessoa sociável' />
-
             <Text
-                variant='tertiary'
+                variant="secondary"
                 forceBold
-                size='2xl'
-                text='Amo trabalhar com tecnologia, não tenho tecnologia específica,
-                sempre estou aberto para novos desafios. Meus maiores foco
-                de conhecimento e gostos são em React Native e Next.JS, junto com
-                o React'
+                size="2xl"
+                text="Me considero uma pessoa sociável"
             />
 
+            <Text
+                variant="tertiary"
+                forceBold
+                size="2xl"
+                text="Amo trabalhar com tecnologia, não tenho tecnologia específica,
+                sempre estou aberto para novos desafios. Meus maiores foco
+                de conhecimento e gostos são em React Native e Next.JS, junto com
+                o React"
+            />
 
             <div className="flex w-full items-center gap-5 pt-5">
                 <div className="min-h-[50px] w-fit bg-white p-2 rounded-md flex items-center">
@@ -73,6 +83,6 @@ export default function About() {
                     />
                 </div>
             </div>
-        </div>
+        </motion.div>
     );
 }
