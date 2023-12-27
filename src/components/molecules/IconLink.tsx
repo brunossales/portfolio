9695a410dd7c095f6f4052c4b9a68743f87a6d7d
Bruno Sales <bruno.particular25@hotmail.com>
2023-12-27 14:43:38 -0300

    fix: lint

--- src/components/molecules/IconLink.tsx
+++ src/components/molecules/IconLink.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic';
 import { LucideProps } from 'lucide-react';
 import dynamicIconImports from 'lucide-react/dynamicIconImports';
 import Link from 'next/link';
@@ -11,9 +11,7 @@ interface IconContainerProps {
 
 const IconLinkContainer = ({ className, children }: IconContainerProps) => {
     return (
-        <div className={`flex gap-2 text-white ${className}`}>
-            {children}
-        </div>
+        <div className={`flex gap-2 text-white ${className}`}>{children}</div>
     );
 };
 
@@ -22,16 +20,15 @@ interface IconProps {
     iconProps?: LucideProps;
 }
 
-
 const Icon = ({ iconProps, icon }: IconProps) => {
     /**
      * Importação dinâmica de ícones
      * parra evitar que todos os ícones sejam importados no build
      */
-    const LucideIcon = dynamic(dynamicIconImports[icon])
+    const LucideIcon = dynamic(dynamicIconImports[icon]);
 
-    return <LucideIcon {...iconProps} />
-}
+    return <LucideIcon {...iconProps} />;
+};
 
 interface LinkProps {
     className?: string;
@@ -41,20 +38,15 @@ interface LinkProps {
 
 const LinkComponent = ({ className, href, label }: LinkProps) => {
     return (
-        <Link
-                className={className}
-                href={href}
-                target="_blank"
-            >
-                {label}
-            </Link>
+        <Link className={className} href={href} target="_blank">
+            {label}
+        </Link>
     );
-}
-
+};
 
 // Composite Pattern
 export const IconLink = {
     Container: IconLinkContainer,
     Icon,
-    Link: LinkComponent
-}
+    Link: LinkComponent,
+};
