a23b87ffc6583781620d28e4e6b512c81c15c35f
Bruno Sales <bruno.particular25@hotmail.com>
2024-01-06 12:08:44 -0300

    test: adding icon test Atom

--- src/components/atoms/Icon.tsx
+++ src/components/atoms/Icon.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic';
 import { LucideProps } from 'lucide-react';
 import dynamicIconImports from 'lucide-react/dynamicIconImports';
 
-interface IconProps extends LucideProps {
+export interface IconProps extends LucideProps {
     name: keyof typeof dynamicIconImports;
 }
 
@@ -12,7 +12,7 @@ const Icon = ({ name, ...props }: IconProps) => {
      * Importação dinâmica de ícones
      * parra evitar que todos os ícones sejam importados no build
      */
-    const LucideIcon = dynamic(dynamicIconImports[name])
+    const LucideIcon = dynamic(dynamicIconImports[name]);
 
     return <LucideIcon {...props} />;
 };
